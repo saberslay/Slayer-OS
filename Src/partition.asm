5777ee539b94f1fcd81e3a0aa69d05142b719727
Saberslay <dansteamshell@gmail.com>
2024-01-22 16:04:10 +0000

    x86-arch

--- Src/partition.asm
+++ Src/partition.asm
@@ -1,13 +1,41 @@
 [org 0x7e00]
 
-mov bx, ReadPartSuccess
-call Printing
+jmp StartProtectMode
 
-jmp $
-
+%include "GDT.asm"
 %include "printing.asm"
 
-ReadPartSuccess:
-    db 'OS: Booting Completed! PartitonReading: Success!', 0
+StartProtectMode:
+    call StartA20
+    cli
+    lgdt [gdt_criptor]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp code:LaunchProtectMode
+
+StartA20:
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+    ret
+
+[bits 32]
+
+LaunchProtectMode:
+    mov ax, data
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov [0xb8000], byte 'T'
+    mov [0xb8002], byte 'E'
+    mov [0xb8004], byte 'S'
+    mov [0xb8006], byte 'T'
+    mov [0xb8008], byte ' '
+
+    jmp $
 
 times 2048-($-$$)db 0
